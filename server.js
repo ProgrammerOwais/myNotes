47f398e065353f142e80dbbab9e398f553b485ad
Muhammad Owais <muhammadowaiskhan616@gmail.com>
2023-08-28 13:40:41 +0500

    new changes

--- server.js
+++ server.js
@@ -59,8 +59,4 @@ mongoose.connection.once("open", () => {
 
 mongoose.connection.on("error", (err) => {
   console.log(err);
-  logEvents(
-    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
-    "mongoErrLog.log"
-  );
 });
