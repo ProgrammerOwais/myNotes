75c5d0f44be5921ed5e93383607fd45905ded3f2
Muhammad Owais <muhammadowaiskhan616@gmail.com>
2023-08-28 13:43:58 +0500

    ok1

--- server.js
+++ server.js
@@ -52,11 +52,11 @@ app.all("*", (req, res) => {
 // custom middleware
 // app.use(errorHandler);
 
-mongoose.connection.once("open", () => {
-  console.log("Connected to MongoDB");
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// mongoose.connection.once("open", () => {
+//   console.log("Connected to MongoDB");
+// });
 
-mongoose.connection.on("error", (err) => {
-  console.log(err);
-});
+// mongoose.connection.on("error", (err) => {
+//   console.log(err);
+// });
